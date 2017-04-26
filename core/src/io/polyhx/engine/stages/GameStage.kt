0cad0d7abd7732f75427e54b7b44ea0742f60d23
Gabrielbdry <gabriel.bdry@gmail.com>
2017-04-25 22:56:47 -0400

    Fix sprite pixelisation

--- core/src/io/polyhx/engine/stages/GameStage.kt
+++ core/src/io/polyhx/engine/stages/GameStage.kt
@@ -1,9 +1,10 @@
 package io.polyhx.engine.stages
 
+import com.badlogic.gdx.scenes.scene2d.Stage
 import io.polyhx.Game
 import io.polyhx.engine.MainGame
 
-class GameStage(var game: MainGame) : com.badlogic.gdx.scenes.scene2d.Stage() {
+class GameStage(var game: MainGame) : Stage() {
 
     var theGame = Game()
 
