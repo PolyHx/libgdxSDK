28fe7437cff059917da205840bebb03c4adffe28
PolyHx <lassondehacks@gmail.com>
2018-05-11 08:48:00 -0400

    Update Game.java

--- core/src/io/polyhx/Game.java
+++ core/src/io/polyhx/Game.java
@@ -13,71 +13,15 @@ import java.util.ArrayList;
 
 public class Game {
 
-    private SpriteEntity background;
-    private AnimatedSpriteEntity bird;
-    private ArrayList<SpriteEntity> obstacles;
-    private Clock clock;
-    private final float spawnRate = 1.0f;
-    private int scrollX;
-    private int scrollSpeed;
-
     public Game() {
-        background = new SpriteEntity("scrollingBackground.png");
-        background.setWrapRepeat();
-        clock = new Clock();
-        bird = new AnimatedSpriteEntity("character.png", 2, 8);
-        obstacles = new ArrayList<SpriteEntity>();
-        scrollX = 0;
-        scrollSpeed = 2;
-    }
-
-    private void generateObstacle() {
-        SpriteEntity tmp = new SpriteEntity("obstacle.png");
-        int x = (int)(Math.random() * Constants.WIDTH);
-        int velocityX = (int)(Math.random() * 40 - 20);
-        int velocityY = (int)(Math.random() * 300 + 100);
-        tmp.setPosition(new Vector2(x, 0));
-        tmp.setVelocity(new Vector2(-velocityX, velocityY));
-        obstacles.add(tmp);
+        
     }
 
     public void act(float delta) {
-        bird.setPosition(Mouse.getPosition());
-
-        if (clock.getElapsedTime() >= 1 / spawnRate) {
-            generateObstacle();
-            clock.restart();
-        }
-
-        for (int i = 0; i < obstacles.size(); i++) {
-            if (obstacles.get(i).getBounds().overlaps(bird.getBounds())) {
-                obstacles.remove(i);
-                i--;
-                scrollSpeed += scrollSpeed;
-            }
-
-            else if (obstacles.get(i).getPosition().y > Constants.HEIGHT) {
-                obstacles.remove(i);
-                i--;
-            }
-        }
-        scrollX += scrollSpeed;
-
-        if (Mouse.isButtonClicked(Input.Buttons.RIGHT)) {
-            bird.pause();
-        } else if (Mouse.isButtonClicked(Input.Buttons.LEFT)) {
-            bird.play();
-        }
+        
     }
 
     public void draw(Batch batch) {
-
-        batch.draw(background.getSprite().getTexture(), 0.0f, 0.0f, scrollX, 0, Constants.WIDTH, Constants.HEIGHT);
-
-        for (SpriteEntity it: obstacles) {
-            it.draw(batch);
-        }
-
-        bird.draw(batch);
+        
     }
 }
